e057938103b0fa8661638ad322cbc7911a90f4bf
FAURE Alexandre <alexandre.faure@u-bordeaux-montaigne.fr>
2025-12-17 11:06:18 +0100

    Sauvegarde temporaire pendant rebase

---
+++ src/app/test-alex/test-alex.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom-test-alex',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-alex.component.html',
+  styleUrl: './test-alex.component.scss'
+})
+export class TestAlexComponent {
+
+}
